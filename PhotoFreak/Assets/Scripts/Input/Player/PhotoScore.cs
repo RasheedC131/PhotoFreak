5b073118cf6c87543fdbcc0fa557f7809614b64d
RasheedC131 <ra357094@ucf.edu>
2026-03-07 18:22:12 -0500

    Implemented Distance and Facing score parameters
    They each give a score from 1-5 I plan to average all the score to give a final result

--- PhotoFreak/Assets/Scripts/Input/Player/PhotoScore.cs
+++ PhotoFreak/Assets/Scripts/Input/Player/PhotoScore.cs
@@ -9,6 +9,86 @@ public class PhotoScore : MonoBehaviour
 
     RaycastHit subject; //what the SphereCast hit
 
+    [Header("Scoring Curves")]
+    public AnimationCurve distanceCurve;
+    public AnimationCurve facingCurve;
+
+    struct ScoreParameters
+    {
+        //Integers representing rating 1-5
+        public int distance; //How far is the subject
+        public int facing; //Is the subject facing the camera
+        public int framing; // How much of the photo does the subject take up
+    };
+
+
+
+    public void CaptureSubject()
+    {
+        ScoreParameters photo = new ScoreParameters();
+
+        if(Physics.SphereCast(transform.position,radius,transform.forward,out subject,maxDistance, layer))
+        {
+           PhotoTag tag = subject.collider.GetComponent<PhotoTag>();
+
+            //For when player takes a picture of a wall or any obstruction
+           if (!tag)
+            {
+                EmptyPhoto(photo);
+                return;
+            }
+
+            photo.distance = DistanceCalculation(transform.position, subject.point);
+            photo.facing = FacingCalculation(transform, subject);
+
+
+
+        } else
+        {
+            EmptyPhoto(photo);
+        }
+
+
+    }
+
+    private int DistanceCalculation(Vector3 playerPos, Vector3 subjectPos)
+    {
+        float distance = Vector3.Distance(playerPos, subjectPos);
+
+        return (int)distanceCurve.Evaluate(distance);
+    }
+
+    private int FacingCalculation(Transform player, RaycastHit subject)
+    {
+        //Angle from subject pov
+        Vector3 fromSubject = subject.collider.transform.forward;
+        Vector3 toPlayer = player.position - subject.point;
+        
+        //Takes horizontals out of the calculation
+        fromSubject.y = 0;
+        toPlayer.y = 0;
+
+        //Convert to unit vectors to stay in bounds
+        fromSubject.Normalize();
+        toPlayer.Normalize();
+        
+        float angle = Vector3.Dot(fromSubject,toPlayer); // 1 in front, 0 next to, -1 behind
+
+        //Debug.Log("Angle: " + angle);
+        //Debug.Log("Angle: " + (int)facingCurve.Evaluate(angle));
+
+        return (int)facingCurve.Evaluate(angle);
+    }
+
+
+
+    private void EmptyPhoto(ScoreParameters photo)
+    {
+        photo.distance = 0;
+        photo.facing = 0;
+        photo.framing = 0;
+    }
+
     //Debug to see SphereCast
     void OnDrawGizmos()
     {
@@ -17,18 +97,4 @@ public class PhotoScore : MonoBehaviour
         Debug.DrawRay(transform.position, transform.forward * maxDistance, Color.green);
     }
 
-    public void CaptureSubject()
-    {
-        if(Physics.SphereCast(transform.position,radius,transform.forward,out subject,maxDistance, layer))
-        {
-           PhotoTag tag = subject.collider.GetComponent<PhotoTag>();
-
-           if (tag) Debug.Log(tag.type);
-
-
-        } else
-        {
-            Debug.Log("oof");
-        }
-    }
 }
