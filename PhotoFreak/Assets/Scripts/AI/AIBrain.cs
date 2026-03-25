e43367b7ea6435813ac4cca86cfa4027ce1379f4
DG-1337 <devgopal745@gmail.com>
2026-03-24 20:26:55 -0400

    WIP: Creating scene for AI to navigate

--- PhotoFreak/Assets/Scripts/AI/AIBrain.cs
+++ PhotoFreak/Assets/Scripts/AI/AIBrain.cs
@@ -1,16 +1,68 @@
+using System.Collections;
 using UnityEngine;
 
 public class AIBrain : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Brain Settings")]
+    public float decisionInterval = 0.25f; 
+    
+    [Header("Debug Info")]
+    public UtilityAction currentAction;
+    public UtilityAction[] availableActions;
+
+    private Coroutine brainCoroutine;
+
     void Start()
     {
-        
+        availableActions = GetComponentsInChildren<UtilityAction>();
+        brainCoroutine = StartCoroutine(BrainTickRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator BrainTickRoutine()
     {
-        
+
+        float randomStartDelay = Random.Range(0f, decisionInterval);
+        yield return new WaitForSeconds(randomStartDelay);
+ 
+        while (true)
+        {
+            ChooseBestAction();
+
+            if (currentAction != null)
+            {
+                currentAction.ExecuteAction();
+            }
+            yield return new WaitForSeconds(decisionInterval);
+        }
+    }
+
+    private void ChooseBestAction()
+    {
+        float highestScore = 0f;
+        UtilityAction bestAction = null;
+
+        foreach (UtilityAction action in availableActions)
+        {
+            float score = action.CalculateUtilityScore();
+
+            if (score > highestScore)
+            {
+                highestScore = score;
+                bestAction = action;
+            }
+        }
+
+        if (bestAction != null && bestAction != currentAction)
+        {
+            currentAction = bestAction;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (brainCoroutine != null)
+        {
+            StopCoroutine(brainCoroutine);
+        }
     }
 }
